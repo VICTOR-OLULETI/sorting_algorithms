69d969434d6c8795012502c643d2a25a246a3f63
VICTOR-OLULETI <oluletiv@gmail.com>
2022-08-17 01:08:11 +0100

    Implmentation of the merge sort in c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,158 +1,61 @@
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * copyArray - copies an array's value into another
- * @A: array to be copied from
- * @B: array to be copied to
- * @iEnd: size of array
- * @iBegin: beginning of array
- *
- * Return: void.
+ * merge_sort_temp - initiate merge sort
+ * @array: array to be sorted
+ * @temp: temporary array for holding sorted elements
+ * @size: size of the array
  */
-
-void copyArray(int *A, size_t iBegin, size_t iEnd, int *B)
+void merge_sort_temp(int *array, int *temp, size_t size)
 {
-	unsigned int k;
+	size_t half = size / 2, i = 0, j = 0, k;
 
-	for (k = iBegin; k < iEnd; k++)
-	{
-		B[k] = A[k];
-	}
-}
+	if (size < 2)
+		return;
 
-/**
- * printMerge - prints the merged elements
- * @A: first array
- * @iBegin: begining of array
- * @iMiddle: middle of array
- * @iEnd: end of array
- * @B: second array
- *
- * Return: void.
- */
+	merge_sort_temp(array, temp, half);
+	merge_sort_temp(array + half, temp + half, size - half);
 
-void printMerge(int *A, size_t iBegin, size_t iMiddle, size_t iEnd, int *B)
-{
-	unsigned int i, k, j;
-	char *sep;
-
-	i = iBegin, j = iMiddle;
 	printf("Merging...\n");
-
-	sep = "";
 	printf("[left]: ");
-	i = iBegin;
-	for (k = iBegin; k < iEnd; k++)
-	{
-		if (i < iMiddle)
-		{
-			printf("%s%d", sep, A[i]);
-			sep = ", ";
-			i += 1;
-		}
-	}
-	printf("\n");
-	sep = "";
+	print_array(array, half);
 	printf("[right]: ");
-	j = iMiddle;
-	for (k = iBegin; k < iEnd; k++)
-	{
-		if (j < iEnd)
+	print_array(array + half, size - half);
+	for (k = 0; k < size; k++)
+		if (j >= size - half || (i < half && array[i] < (array + half)[j]))
 		{
-			printf("%s%d", sep, A[j]);
-			sep = ", ";
-			j += 1;
-		}
-	}
-	printf("\n");
-	sep = "";
-	printf("[Done]: ");
-	for (k = iBegin; k < iEnd; k++)
-	{
-		printf("%s%d", sep, B[k]);
-		sep = ", ";
-	}
-	printf("\n");
-}
-/**
- * topDownMerge - merges the top down split
- *
- * @A: first array
- * @iBegin: beginning of array
- * @iMiddle: middle of array
- * @iEnd: End of array
- * @B: second array
- *
- * Return: void.
- */
-
-void topDownMerge(int *A, size_t iBegin, size_t iMiddle, size_t iEnd, int *B)
-{
-	unsigned int i, k, j;
-
-	printMerge(A, iBegin, iMiddle, iEnd, B);
-	i = iBegin, j = iMiddle;
-	for (k = iBegin; k < iEnd; k++)
-	{
-
-		if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
-		{
-			B[k] = A[i];
-			i = i + 1;
+			temp[k] = array[i];
+			i++;
 		}
 		else
 		{
-			B[k] = A[j];
-			j = j + 1;
+			temp[k] = (array + half)[j];
+			j++;
 		}
-	}
+	for (k = 0; k < size; k++)
+		array[k] = temp[k];
+	printf("[Done]: ");
+	print_array(array, size);
 }
 
 /**
- * topDownSplitMerge - implements the top down split technique
- * for merge sort algorithm
- *
- * @B: second array
- * @iBegin: beginning of array
- * @iEnd: end of array
- * @A: first array
- *
- * Return: void
+ * merge_sort - initiate merge sort
+ * @array: array to be sorted
+ * @size: size of the array
  */
-
-void topDownSplitMerge(int *B, size_t iBegin, size_t iEnd, int *A)
+void merge_sort(int *array, size_t size)
 {
-	size_t iMiddle;
+	int *temp_array;
 
-	if (iEnd - iBegin <= 1)
-		return;
-	iMiddle = (iEnd + iBegin) / 2;
-
-	topDownSplitMerge(A, iBegin, iMiddle, B);
-	topDownSplitMerge(A, iMiddle, iEnd, B);
-	topDownMerge(B, iBegin, iMiddle, iEnd, A);
-}
-
-/**
- * merge_sort - sorts an array of integers in ascending order
- * using the Merge sort algorithm
- * @arrayA: array of integers
- * @size: size of @array
- *
- * Return: void.
- */
-
-void merge_sort(int *arrayA, size_t size)
-{
-	int *arrayB;
-
-	if (!arrayA || size < 2)
+	if (!array || size < 2)
 		return;
 
-	arrayB = malloc(sizeof(int) * size);
-	if (arrayB == NULL)
+	temp_array = malloc(sizeof(*temp_array) * size);
+	if (!temp_array)
 		return;
-	copyArray(arrayA, 0, size, arrayB);
-	topDownSplitMerge(arrayB, 0, size, arrayA);
-	free(arrayB);
+
+	merge_sort_temp(array, temp_array, size);
+	free(temp_array);
 }
